85207a6bf9a4ad7a5cbf294aaa67892566103c19
Dexter(노영삼) <nys@gabia.com>
2021-06-14 09:33:05 +0900

    project init

---
+++ src/test/java/com/gabia/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
