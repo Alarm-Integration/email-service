2dcf21447033ae993841a8d39338b9bcfdad0d20
Dexter(노영삼) <nys@gabia.com>
2021-06-15 09:29:05 +0900

    fix: @Value -> @EnableConfigurationProperties
    * Type-safe Configuration Properties 기능으로 변경
    * org.springframework.boot:spring-boot-configuration-processor 의존성 추가
    * application.properties -> application.yml 변경

--- src/main/java/com/gabia/emailservice/config/AmazonConfig.java
+++ src/main/java/com/gabia/emailservice/config/AmazonConfig.java
@@ -4,22 +4,21 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
-
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableConfigurationProperties(AwsSesProperties.class)
+@RequiredArgsConstructor
 @Configuration
-public class AwsSesConfig {
-    @Value("${aws.ses.credentials.access-key}")
-    private String accessKey;
+public class AmazonConfig {
 
-    @Value("${aws.ses.credentials.secret-key}")
-    private String secretKey;
+    private final AwsSesProperties awsSesProperties;
 
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(awsSesProperties.getAccessKey(), awsSesProperties.getSecretKey());
         AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(basicAWSCredentials);
 
         return AmazonSimpleEmailServiceClientBuilder.standard()
---
+++ src/main/java/com/gabia/emailservice/config/AwsSesProperties.java
@@ -0,0 +1,15 @@
+package com.gabia.emailservice.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConstructorBinding
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "aws.ses.credentials")
+public class AwsSesProperties {
+    private final String accessKey;
+    private final String secretKey;
+}
