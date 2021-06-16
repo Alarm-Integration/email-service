08c4c7dabdd34f327eaa924a880ae9dab84566ed
Dexter(노영삼) <nys@gabia.com>
2021-06-16 14:51:46 +0900

    fix: SendEmailRequest 필드명 변경

--- src/main/java/com/gabia/emailservice/dto/request/SendEmailRequest.java
+++ src/main/java/com/gabia/emailservice/dto/request/SendEmailRequest.java
@@ -4,38 +4,36 @@ import com.amazonaws.services.simpleemail.model.Body;
 import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Getter
+@Data
 @NoArgsConstructor
 public class SendEmailRequest {
-    private String from;
-    private List<String> to;
-    private String subject;
+    private String sender;
+    private List<String> raws;
+    private String title;
     private String content;
 
     @Builder
-    public SendEmailRequest(String from, List<String> to, String subject, String content) {
-        this.from = from;
-        this.to = to;
-        this.subject = subject;
+    public SendEmailRequest(String sender, List<String> raws, String title, String content) {
+        this.sender = sender;
+        this.raws = raws;
+        this.title = title;
         this.content = content;
     }
 
     public com.amazonaws.services.simpleemail.model.SendEmailRequest toSendRequestDto() {
         Destination destination = new Destination()
-                .withToAddresses(this.to);
+                .withToAddresses(this.raws);
 
         Message message = new Message()
-                .withSubject(createContent(this.subject))
+                .withSubject(createContent(this.title))
                 .withBody(new Body().withHtml(createContent(this.content)));
 
         return new com.amazonaws.services.simpleemail.model.SendEmailRequest()
-                .withSource(this.from)
+                .withSource(this.sender)
                 .withDestination(destination)
                 .withMessage(message);
     }
