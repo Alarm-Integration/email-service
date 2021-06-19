ae42e72d967810970c1b16bcafbb36a1b875fd87
Dexter(노영삼) <nys@gabia.com>
2021-06-19 12:49:31 +0900

    test: AmazonEmailSenderTest 작성 - 메일_발송_성공 - 메일_발송_실패_인증하지_않은_발신자 - 인증_메일_발송_성공

--- src/test/java/com/gabia/emailservice/sender/AmazonEmailSenderTest.java
+++ src/test/java/com/gabia/emailservice/sender/AmazonEmailSenderTest.java
@@ -1,13 +1,13 @@
 package com.gabia.emailservice.sender;
 
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.MessageRejectedException;
 import com.amazonaws.services.simpleemail.model.SendEmailResult;
 import com.amazonaws.services.simpleemail.model.VerifyEmailIdentityRequest;
 import com.amazonaws.services.simpleemail.model.VerifyEmailIdentityResult;
 import com.gabia.emailservice.dto.request.SendEmailRequest;
 import com.gabia.emailservice.dto.response.SendEmailResponse;
 import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,9 +27,8 @@ class AmazonEmailSenderTest {
     @InjectMocks
     private AmazonEmailSender amazonEmailSender;
 
-    @DisplayName("AWS SES email 전송 테스트")
     @Test
-    void send_email(){
+    void 메일_발송_성공(){
         //given
         SendEmailRequest request = SendEmailRequest.builder()
                 .sender("nameks17@gmail.com")
@@ -39,10 +38,11 @@ class AmazonEmailSenderTest {
                 .build();
 
         SendEmailResponse response = SendEmailResponse.builder()
-                .message("123")
+                .message("메일 발송 완료")
                 .build();
 
-        given(amazonSimpleEmailService.sendEmail(request.toSendRequestDto())).willReturn(new SendEmailResult().withMessageId("123"));
+        given(amazonSimpleEmailService.sendEmail(request.toSendRequestDto()))
+                .willReturn(new SendEmailResult().withMessageId(response.getMessage()));
 
         //when
         SendEmailResponse result = amazonEmailSender.sendEmail(request);
@@ -51,25 +51,40 @@ class AmazonEmailSenderTest {
         assertThat(result.getMessage()).isEqualTo(response.getMessage());
     }
 
-    @DisplayName("AWS SES 인증 이메일 전송 테스트")
     @Test
-    void sendVerifyEmail() {
+    void 메일_발송_실패_인증하지_않은_발신자(){
+        //given
+        SendEmailRequest request = SendEmailRequest.builder()
+                .sender("notverified@email.com")
+                .raws(Lists.newArrayList("nameks@naver.com"))
+                .title("테스트")
+                .content("안녕하세요")
+                .build();
+
+        given(amazonSimpleEmailService.sendEmail(request.toSendRequestDto()))
+                .willThrow(new MessageRejectedException("Email address is not verified"));
+
+        //when
+        SendEmailResponse sendEmailResponse = amazonEmailSender.sendEmail(request);
+
+        //then
+        assertThat(sendEmailResponse.getMessage()).isEqualTo("Email address is not verified");
+    }
+
+    @Test
+    void 인증_메일_발송_성공() {
         //given
         String emailAddress = "nameks@naver.com";
 
         VerifyEmailIdentityResult verifyEmailIdentityResult = mock(VerifyEmailIdentityResult.class);
-        
+
         given(amazonSimpleEmailService.verifyEmailIdentity(new VerifyEmailIdentityRequest().withEmailAddress(emailAddress)))
                 .willReturn(verifyEmailIdentityResult);
-
-        SendEmailResponse response = SendEmailResponse.builder()
-                .message("ok")
-                .build();
 
         //when
         SendEmailResponse result = amazonEmailSender.sendVerifyEmail(emailAddress);
 
         //then
-        assertThat(result.getMessage()).isEqualTo(response.getMessage());
+        assertThat(result.getMessage()).isEqualTo("인증 메일 발송 완료");
     }
 }
