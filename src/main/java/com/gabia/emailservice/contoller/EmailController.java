b682016968a9fea2031ac8c441af60f683df7145
Dexter(노영삼) <nys@gabia.com>
2021-06-23 13:01:59 +0900

    test8

--- src/main/java/com/gabia/emailservice/contoller/EmailController.java
+++ src/main/java/com/gabia/emailservice/contoller/EmailController.java
@@ -3,11 +3,14 @@ package com.gabia.emailservice.contoller;
 import com.gabia.emailservice.dto.response.APIResponse;
 import com.gabia.emailservice.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class EmailController {
@@ -19,6 +22,10 @@ public class EmailController {
         emailService.sendVerifyEmail(email);
 
         return ResponseEntity.ok(APIResponse.withMessage("인증 메일 발송 완료"));
+    }
+
+    public void test(){
+        log.info("test code");
     }
 
 }
