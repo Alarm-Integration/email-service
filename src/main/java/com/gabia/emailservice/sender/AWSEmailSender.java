33b50a9f63a9e9b5eabe958886312accf170a2b9
Dexter(노영삼) <nys@gabia.com>
2021-06-23 09:46:22 +0900

    modify: AWSEmailSender 수정 - 테스트 통과를 위한 수정

--- src/main/java/com/gabia/emailservice/sender/AWSEmailSender.java
+++ src/main/java/com/gabia/emailservice/sender/AWSEmailSender.java
@@ -3,7 +3,6 @@ package com.gabia.emailservice.sender;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.VerifyEmailIdentityRequest;
 import com.gabia.emailservice.dto.request.SendEmailRequest;
-import com.gabia.emailservice.dto.response.SendEmailResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,29 +15,20 @@ public class AWSEmailSender implements CommonEmailSender {
     private final AmazonSimpleEmailService amazonSimpleEmailService;
 
     @Override
-    public SendEmailResponse sendEmail(SendEmailRequest request){
+    public void sendEmail(SendEmailRequest request) throws Exception {
         try {
-            amazonSimpleEmailService.sendEmail(request.toSendRequestDto());
+            amazonSimpleEmailService.sendEmail(request.toAWSRequest());
+        } catch (Exception e) {
+            if (e.getMessage().startsWith("Email address is not verified"))
+                throw new Exception("인증된 발신자가 아닙니다.");
+            throw new Exception(e.getMessage());
         }
-        catch (Exception e){
-            log.error("send email error message: {}",e.getMessage());
-
-            if(e.getMessage().startsWith("Email address is not verified"))
-                return SendEmailResponse.withMessage("Email address is not verified");
-
-            return SendEmailResponse.withMessage(e.getMessage());
-        }
-
-        return SendEmailResponse.withMessage("메일 발송 완료");
     }
 
     @Override
-    public SendEmailResponse sendVerifyEmail(String emailAddress) {
+    public void sendVerifyEmail(String emailAddress) {
         VerifyEmailIdentityRequest request = new VerifyEmailIdentityRequest().withEmailAddress(emailAddress);
-
         amazonSimpleEmailService.verifyEmailIdentity(request);
-
-        return SendEmailResponse.withMessage("인증 메일 발송 완료");
     }
 }
 
