867e5cd0fcd4b1587c1869424f5c514af129b64f
Dexter(노영삼) <nys@gabia.com>
2021-06-23 14:07:05 +0900

    test12

--- src/main/java/com/gabia/emailservice/contoller/EmailController.java
+++ src/main/java/com/gabia/emailservice/contoller/EmailController.java
@@ -3,13 +3,11 @@ package com.gabia.emailservice.contoller;
 import com.gabia.emailservice.dto.response.APIResponse;
 import com.gabia.emailservice.service.EmailService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class EmailController {
@@ -18,7 +16,6 @@ public class EmailController {
 
     @PostMapping("/verify-email/{email}")
     public ResponseEntity<?> sendVerifyEmail(@PathVariable String email) {
-        log.info("test code2");
         emailService.sendVerifyEmail(email);
 
         return ResponseEntity.ok(APIResponse.withMessage("인증 메일 발송 완료"));
