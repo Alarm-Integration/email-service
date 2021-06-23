131cf69f9199680321169d493995449d6bff5cbd
Dexter(노영삼) <nys@gabia.com>
2021-06-23 09:46:47 +0900

    modify: CommonEmailSender 수정 - 메소드 반환 void로 변경

--- src/main/java/com/gabia/emailservice/sender/CommonEmailSender.java
+++ src/main/java/com/gabia/emailservice/sender/CommonEmailSender.java
@@ -1,12 +1,11 @@
 package com.gabia.emailservice.sender;
 
 import com.gabia.emailservice.dto.request.SendEmailRequest;
-import com.gabia.emailservice.dto.response.SendEmailResponse;
 
 public interface CommonEmailSender {
-    SendEmailResponse sendEmail(SendEmailRequest sendEmailRequest);
+    void sendEmail(SendEmailRequest sendEmailRequest) throws Exception;
 
-    default SendEmailResponse sendVerifyEmail(String emailAddress) {
-        return null;
+    default void sendVerifyEmail(String emailAddress) {
+
     }
 }
