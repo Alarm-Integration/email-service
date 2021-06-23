20d5ed253cc2cabef5ddebe60d4eaadf72018f42
Dexter(노영삼) <nys@gabia.com>
2021-06-23 09:47:54 +0900

    modify: EmailService 수정 - exception 처리 - log 출력 추가

--- src/main/java/com/gabia/emailservice/service/EmailService.java
+++ src/main/java/com/gabia/emailservice/service/EmailService.java
@@ -1,12 +1,13 @@
 package com.gabia.emailservice.service;
 
 import com.gabia.emailservice.dto.request.SendEmailRequest;
-import com.gabia.emailservice.dto.response.SendEmailResponse;
 import com.gabia.emailservice.sender.CommonEmailSender;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class EmailService {
@@ -14,11 +15,17 @@ public class EmailService {
     private final CommonEmailSender commonEmailSender;
 
     @KafkaListener(topics = "email", groupId = "email", containerFactory = "emailListenerContainerFactory")
-    public SendEmailResponse sendEmail(SendEmailRequest sendEmailRequest) {
-        return commonEmailSender.sendEmail(sendEmailRequest);
+    public void sendEmail(SendEmailRequest sendEmailRequest) {
+        try {
+            commonEmailSender.sendEmail(sendEmailRequest);
+            log.info("메일 발송 완료");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 
-    public SendEmailResponse sendVerifyEmail(String emailAddress) {
-        return commonEmailSender.sendVerifyEmail(emailAddress);
+    public void sendVerifyEmail(String emailAddress) {
+        commonEmailSender.sendVerifyEmail(emailAddress);
+        log.info("인증 메일 발송 완료");
     }
 }
