0798a3e55b8e78009ebd9e9948d5fa7f1e314b2a
Dexter(노영삼) <nys@gabia.com>
2021-06-29 09:01:03 +0900

    modify: AWSEmailSender 수정 - 성공시 로그 출력 - 실패시 로그 출력 및 예외 던지기

--- src/main/java/com/gabia/emailservice/sender/AWSEmailSender.java
+++ src/main/java/com/gabia/emailservice/sender/AWSEmailSender.java
@@ -1,6 +1,7 @@
 package com.gabia.emailservice.sender;
 
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.SendEmailResult;
 import com.amazonaws.services.simpleemail.model.VerifyEmailIdentityRequest;
 import com.gabia.emailservice.dto.request.SendEmailRequest;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,14 @@ public class AWSEmailSender implements CommonEmailSender {
     @Override
     public void sendEmail(SendEmailRequest request) throws Exception {
         try {
-            amazonSimpleEmailService.sendEmail(request.toAWSRequest());
+            SendEmailResult sendEmailResult = amazonSimpleEmailService.sendEmail(request.toAWSRequest());
+            log.info(String.format("AWSEmailSender: 발송 성공 %s", sendEmailResult.getMessageId()));
         } catch (Exception e) {
-            if (e.getMessage().startsWith("Email address is not verified"))
-                throw new Exception("인증된 발신자가 아닙니다.");
+            if (e.getMessage().startsWith("Email address is not verified")) {
+                log.error(String.format("AWSEmailSender: 인증된 발신자(%s)가 아닙니다", request.getSender()));
+                throw new Exception("인증된 발신자가 아닙니다");
+            }
+            log.error(String.format("AWSEmailSender: %s", e.getMessage()));
             throw new Exception(e.getMessage());
         }
     }
@@ -29,6 +34,7 @@ public class AWSEmailSender implements CommonEmailSender {
     public void sendVerifyEmail(String emailAddress) {
         VerifyEmailIdentityRequest request = new VerifyEmailIdentityRequest().withEmailAddress(emailAddress);
         amazonSimpleEmailService.verifyEmailIdentity(request);
+        log.info("AWSEmailSender: 인증 메일 발송 성공");
     }
 }
 
